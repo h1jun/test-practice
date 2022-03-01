d46778f890efd40ebaac468bb034f7bc01f36049
Wonjun <dittoavec@gmail.com>
2022-03-02 02:29:08 +0900

    Add state and event test code

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import Greeting from "./Greeting";
+import userEvent from "@testing-library/user-event"; //userEvent : 실제 화면에서 사용자 이벤트를 작동시키도록 돕는 객체
 import { render, screen } from "@testing-library/react";
 
 describe("Greeting Component", () => {
@@ -12,5 +13,41 @@ describe("Greeting Component", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World!");
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("renders  good to see you if the button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("renders Changed! if the button was  clicked", () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('does not render "good to see you" if the buttons was clicked', () => {
+    // Arrange
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+
+    // Assert
+    //
+    //queryByText는 엘리먼트를 찾지 못한다면 null을 반환한다.
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
